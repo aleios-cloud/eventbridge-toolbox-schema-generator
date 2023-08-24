63ed0c4dceee7f0ae36513a49e765964bbcf77bd
April Bates <aprilb@aleios.com>
2023-08-24 17:22:12 +0100

    feat: test for generateDocs

--- src/__tests__/generate-docs.test.ts
+++ src/__tests__/generate-docs.test.ts
@@ -1,5 +1,11 @@
-import { describe, expect, it, vi } from "vitest";
-import { getNewDocumentationFilePath, getOldDocumentationFilePath } from "../generate-docs";
+import { MockedFunction, describe, expect, it, vi } from "vitest";
+import {
+  generateDocumentation,
+  getNewDocumentationFilePath,
+  getOldDocumentationFilePath,
+} from "../generate-docs";
+import { generateAllContractInformation } from "../helpers/generateAllContractInformation";
+import { writeDocumentation } from "../helpers/writeDocumentation";
 
 describe("Given a generate docs script", () => {
   describe("With function getNewDocumentationFilePath to generate filepaths", () => {
@@ -13,31 +19,90 @@ describe("Given a generate docs script", () => {
     });
   });
 
-    describe("With function getOldDocumentationFilePath to generate filepaths", () => {
-      it("Returns a filepath based on input", async () => {
-        expect(
-          getOldDocumentationFilePath(
-            "mockPathToDocumentationFolder",
-            "mockDetailType",
-            100
-          )
-        ).toStrictEqual(
-          "mockPathToDocumentationFolder/mockDetailType/versioned/100"
-        );
+  describe("With function getOldDocumentationFilePath to generate filepaths", () => {
+    it("Returns a filepath based on input", async () => {
+      expect(
+        getOldDocumentationFilePath(
+          "mockPathToDocumentationFolder",
+          "mockDetailType",
+          100
+        )
+      ).toStrictEqual(
+        "mockPathToDocumentationFolder/mockDetailType/versioned/100"
+      );
+    });
+  });
+
+  describe("With function compileContractInformation to compile information from the contracts", () => {
+    it("Returns schema details and newest version information for a set of contracts", async () => {
+      expect(
+        getOldDocumentationFilePath(
+          "mockPathToDocumentationFolder",
+          "mockDetailType",
+          100
+        )
+      ).toStrictEqual(
+        "mockPathToDocumentationFolder/mockDetailType/versioned/100"
+      );
+    });
+  });
+
+  vi.mock("../helpers/generateAllContractInformation");
+  const mockGenerateAllContractInformation =
+    generateAllContractInformation as MockedFunction<
+      typeof generateAllContractInformation
+    >;
+  const mockSchemaDetails = {
+    detailType: "mockDetailTypeConst",
+    detailVersion: 10,
+    schema: {
+      properties: {
+        "detail-type": {
+          const: "mockDetailTypeConst",
+        },
+        detail: {
+          properties: {
+            "detail-version": {
+              const: 10,
+            },
+          },
+        },
+      },
+    },
+  };
+
+  vi.mock("../helpers/writeDocumentation");
+  const mockWriteDocumentation = writeDocumentation as MockedFunction<
+    typeof writeDocumentation
+  >;
+
+  describe("Which handles the creating on documentation", () => {
+    it("Calls writeDocumentation with a versioned filepath if handling an old contract", async () => {
+      mockGenerateAllContractInformation.mockResolvedValueOnce({
+        allSchemaDetails: [mockSchemaDetails],
+        newestVersionsRecord: { mockDetailTypeConst: 100 },
       });
+      await generateDocumentation(
+        "mockPathToContractsFolder",
+        "mockPathToDocumentationFolder"
+      );
+      expect(mockWriteDocumentation).toBeCalledWith(
+        mockSchemaDetails, `mockPathToDocumentationFolder/mockDetailTypeConst/versioned/10`
+      );
     });
 
-    describe("With function compileContractInformation to compile information from the contracts", () => {
-      it("Returns schema details and newest version information for a set of contracts", async () => {
-        expect(
-          getOldDocumentationFilePath(
-            "mockPathToDocumentationFolder",
-            "mockDetailType",
-            100
-          )
-        ).toStrictEqual(
-          "mockPathToDocumentationFolder/mockDetailType/versioned/100"
-        );
+    it("Calls writeDocumentation with a unversioned filepath if handling a new contract", async () => {
+      mockGenerateAllContractInformation.mockResolvedValueOnce({
+        allSchemaDetails: [mockSchemaDetails],
+        newestVersionsRecord: { mockDetailTypeConst: 10 },
       });
+      await generateDocumentation(
+        "mockPathToContractsFolder",
+        "mockPathToDocumentationFolder"
+      );
+      expect(mockWriteDocumentation).toBeCalledWith(
+        mockSchemaDetails, `mockPathToDocumentationFolder/mockDetailTypeConst`
+      );
     });
+  });
 });
