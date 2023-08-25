7038fd4b407e23483e339a270dd8c391cbb19e49
April Bates <aprilb@aleios.com>
2023-08-25 17:31:11 +0100

    feat: creating integration test

--- src/__tests__/generateDocs.integration.test.ts
+++ src/__tests__/generateDocs.integration.test.ts
@@ -1,34 +1,57 @@
-import { describe, expect, it, vi, afterEach, beforeEach } from "vitest";
-import mock from "mock-fs";
+import { describe, expect, it, afterEach } from "vitest";
 import { generateDocumentation } from "../generateDocs";
 import path from "path";
+import { rimraf } from "rimraf";
+import { readdir } from "fs/promises";
+import { existsSync, readFileSync } from "fs";
 
-const mockFileSystem = {
-  "mock/contracts/directory": {
-    "testContract.ts": mock.load(
-      path.join(
-        process.cwd(),
-        "/example-architecture/events/contracts/personRegisteredContractV1.ts"
-      )
-    ),
-    "testContract2.ts": mock.load(
-      path.join(
-        process.cwd(),
-        "/example-architecture/events/contracts/personRegisteredContractV2.ts"
-      )
-    ),
-  },
-  "mock/events/directory": {},
-};
+const testContractsPath = path.join(
+  process.cwd(),
+  "/src/__tests__/fixtures/contracts"
+);
+const testDocsPath = path.join(process.cwd(), "/src/__tests__/fixtures/docs");
 
 describe("Create documentation for contract types, standardised for use with event catalog", () => {
-  beforeEach(function () {
-    mock(mockFileSystem);
+  afterEach(async () => {
+    const files = await readdir(testDocsPath);
+    for (const file of files) {
+      await rimraf(path.join(testDocsPath, file));
+    }
   });
-  afterEach(mock.restore);
 
-  it("Creates documentation without error when passed valid contract types", () => {
-    generateDocumentation("mock/contracts/directory", "mock/events/directory");
-    expect(true);
+  it("Creates documentation without error when passed valid contract types", async () => {
+    await generateDocumentation(testContractsPath, testDocsPath);
+    var savedVersionedDocs = readFileSync(
+      path.join(
+        testDocsPath,
+        "/PersonRegisteredContract/versioned/1/schema.json"
+      ),
+      {
+        encoding: "utf8",
+      }
+    );
+    expect(savedVersionedDocs).toMatch(/("const": "PersonRegisteredContract")/);
+    expect(savedVersionedDocs).toMatch(/("const": 1)/);
+    expect(
+      existsSync(
+        path.join(
+          testDocsPath,
+          "/PersonRegisteredContract/versioned/1/index.md"
+        )
+      )
+    ).toStrictEqual(true);
+    var savedUnversionedDocs = readFileSync(
+      path.join(testDocsPath, "/PersonRegisteredContract/schema.json"),
+      {
+        encoding: "utf8",
+      }
+    );
+    expect(savedUnversionedDocs).toMatch(
+      /("const": "PersonRegisteredContract")/i
+    );
+    expect(savedUnversionedDocs).toMatch(/("const": 2)/i);
+    expect(
+      existsSync(path.join(testDocsPath, "/PersonRegisteredContract/index.md"))
+    ).toStrictEqual(true);
   });
 });
